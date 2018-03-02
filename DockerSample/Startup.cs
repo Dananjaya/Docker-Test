9798d30201dd2bbfb447272adbb66c87f1b352d1
dananjaya@outlook.com <dananjaya@outlook.com>
2018-03-02 15:21:42 +0530

    intial commit
    Initial Commit

---
+++ DockerSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DockerSample.Startup))]
+namespace DockerSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
